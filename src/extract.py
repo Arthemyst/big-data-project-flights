0fd292e76e2a85a913138bb9c796aef366f7fc72
Arthemyst <pawcio262@gmail.com>
2025-03-26 12:03:35 +0100

    created fetch_flight_data and save_to_s3 functions

--- src/extract.py
+++ src/extract.py
@@ -1,19 +1,54 @@
-import pandas as pd
-import requests
+import json
 import os
-import pathlib
-url = "https://opensky-network.org/api/states/all"
-response = requests.get(url)
-data = response.json()
-data_short = list()
-for row in data["states"]:
-    data_short.append(row[:7])
+from datetime import datetime
 
-df = pd.DataFrame(data_short,
-                  columns=["icao24", "callsign", "origin_country", "time_position", "last_contact", "longitude",
-                           "latitude"])
+import boto3
+import requests
 
-data_dir = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), "data")
-if not os.path.exists(data_dir):
-    os.mkdir(data_dir)
-df.to_csv(os.path.join(data_dir, "flights.csv"), index=False)
+AWS_REGION = os.getenv("AWS_REGION")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+DATA_FOLDER = "raw_files"
+API_URL = "https://opensky-network.org/api/states/all"
+s3_client = boto3.client("s3", region_name=AWS_REGION)
+
+
+def fetch_flight_data() -> dict or None:
+    response = requests.get(API_URL)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Error during data downloading: {response.status_code}")
+        return None
+
+
+def save_to_s3(data: dict) -> None:
+    if not data:
+        print("No data to save.")
+        return
+
+    timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = f"{DATA_FOLDER}/flights_{timestamp}.json"
+
+    json_data = json.dumps(data, indent=4)
+
+    try:
+        s3_client.put_object(
+            Bucket=S3_BUCKET_NAME,
+            Key=file_name,
+            Body=json_data,
+            ContentType="application/json"
+        )
+        print(f"File saved in S3: s3://{S3_BUCKET_NAME}//{file_name}")
+    except Exception as e:
+        print(f"Error during saving to S3: {e}")
+
+
+if __name__ == "__main__":
+    print("Downloading flights data...")
+    flight_data = fetch_flight_data()
+
+    if flight_data:
+        print("Saving data to S3...")
+        save_to_s3(flight_data)
